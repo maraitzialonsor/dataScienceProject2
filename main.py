1dd9beba4b9bacea6616e52d3ef85a68a1b1e3d9
Maraitzi <mara00@gmail.com gitgit config --global user.email mara00@gmail.com>
2022-02-27 14:40:51 -0600

    Imports/Exporst

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import pandas as pd
 # %%
 df = pd.read_csv('synergy_logistics_database.csv')
 # %%
-df.groupby('transport_mode').describe()['total_value']
+#df.groupby('transport_mode').describe()['total_value']
 # %%
 imports = df[df['direction'] == 'Imports']
-print(imports)
 # %%
 exports = df[df['direction'] == 'Exports']
-print(imports)
 # %%
-rutas = exports.groupby(['origin', 'destination','transport_mode'])
+rutas_export = exports.groupby(['origin', 'destination','transport_mode'])
 # %%
-rutas.count()['total_value'].sort_values(ascending=False)
-
+rutas_export.count()['total_value'].sort_values(ascending=False).head(10)
 # %%
+rutas_import = imports.groupby(['origin', 'destination','transport_mode'])
+# %%
+# %%
+rutas_import.count()['total_value'].sort_values(ascending=False).head(10)
