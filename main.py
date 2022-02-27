cb696bb4855a257630ba8b2bbebfb3734ee16b38
Maraitzi <mara00@gmail.com gitgit config --global user.email mara00@gmail.com>
2022-02-26 23:44:51 -0600

    Exports

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+# %% [markdown]
+# # Proyecto 2
 #%%
 import pandas as pd
 # %%
 df = pd.read_csv('synergy_logistics_database.csv')
 # %%
 df.groupby('transport_mode').describe()['total_value']
+# %%
+imports = df[df['direction'] == 'Imports']
+print(imports)
+# %%
+exports = df[df['direction'] == 'Exports']
+print(imports)
+# %%
+rutas = exports.groupby(['origin', 'destination','transport_mode'])
+# %%
+rutas.count()['total_value'].sort_values(ascending=False)
+
 # %%
