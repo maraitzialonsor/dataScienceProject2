0952117a14e1c60a56b05bdaeb4a189a62da8a0f
Maraitzi <mara00@gmail.com gitgit config --global user.email mara00@gmail.com>
2022-03-05 19:15:51 -0600

    v0.2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # %% [markdown]
 # # Proyecto 2
 #%%
+# Imports
 import pandas as pd
+import matplotlib.pyplot as plt
 # %%
 df = pd.read_csv('synergy_logistics_database.csv')
 # %%
@@ -18,14 +20,27 @@ rutas_import.count()['total_value'].sort_values(ascending=False).head(10)
 
 # %%
 # Medio de transporte utilizado en importaciones
-imports.groupby(by=['transport_mode']).sum().groupby(level=[0]).cumsum()['total_value'].sort_values(ascending=False)
+imports_transport = imports.groupby(by=['transport_mode']).sum().groupby(level=[0]).cumsum()['total_value'].sort_values(ascending=False)
 # %%
 # Medio de transporte utilizado en exportaciones
-exports.groupby(by=['transport_mode']).sum().groupby(level=[0]).cumsum()['total_value'].sort_values(ascending=False)
+exports_transport = exports.groupby(by=['transport_mode']).sum().groupby(level=[0]).cumsum()['total_value'].sort_values(ascending=False)
 # %%
 # Obtenemos el total_value de importaciones y exportaciones
 total_imports = int(imports['total_value'].sum())
 total_exports = int(exports['total_value'].sum())
+
+# %%
+# Gráficas de resultados medios de transporte 
+plt.style.use("bmh")                   
+plt.bar(imports_transport.index, imports_transport) 
+plt.show()                               
+plt.close('all')
+# %%
+# Gráficas de resultados medios de transporte                 
+plt.bar(exports_transport.index, exports_transport) 
+plt.show()                               
+plt.close('all')
+
 # %%
 # Nuevo dataframe para obtener porcentajes de importaciones por país
 # Frecuencia acumulada de importaciones 
@@ -39,5 +54,3 @@ exports_pais = pd.DataFrame(exports.groupby(by=['origin']).sum().groupby(level=[
 exports_pais['porcentaje'] = exports_pais['total_value']*100/total_exports
 exports_pais['acum'] = (exports_pais['porcentaje']).cumsum()
 print(exports_pais[:8])
-
-# %%
